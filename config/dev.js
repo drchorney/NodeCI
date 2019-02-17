29943562325946bf43128a5a567f0943ed02f0ca
Drew Chorney <drchorne@gmail.com>
2019-02-16 18:50:22 -0700

    fixed small configuration error

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,5 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://localhost:27021/blog_everyone',
   cookieKey: '123123123',
-  redisUrl = 'redis://127.0.0.1:6379'
+  redisUrl: 'redis://127.0.0.1:6379'
 };
